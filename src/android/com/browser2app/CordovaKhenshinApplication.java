09179bf70859c345e7bfa6562fde348a1b843baf
Luis Hernan Paul <lhpaul11@gmail.com>
2017-08-28 17:22:44 -0300

    plugin configuration working for android

--- src/android/com/browser2app/CordovaKhenshinApplication.java
+++ src/android/com/browser2app/CordovaKhenshinApplication.java
@@ -1,17 +1,19 @@
 package com.browser2app;
 
 import android.app.Application;
-import android.os.Build;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
 
 import com.browser2app.khenshin.Khenshin;
 import com.browser2app.khenshin.KhenshinInterface;
 import com.browser2app.khenshin.KhenshinApplication;
-import com.browser2app.khenshin.automaton.WebClient;
 
 public class CordovaKhenshinApplication extends Application implements KhenshinApplication{
 
+	private static final String TAG = "KhenshinPlugin";
 	private Khenshin khenshin;
 
 	@Override
@@ -19,20 +21,45 @@ public class CordovaKhenshinApplication extends Application implements KhenshinA
 		return khenshin;
 	}
 
-	public CordovaKhenshinApplication() {
+    @Override
+    public void onCreate() {
+        try {
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            String taskApiUrl = bundle.getString("com.browser2app.TASK_API_URL");
+            String dumpApiUrl = bundle.getString("com.browser2app.DUMP_API_URL");
+            String mainButtonStyle = bundle.getString("com.browser2app.MAIN_BUTTON_STYLE");
+            boolean allowCredentialsSaving = bundle.getBoolean("com.browser2app.ALLOW_CREDENTIALS_SAVING", false);
+            boolean hideWebAddress = bundle.getBoolean("com.browser2app.HIDE_WEB_ADDRESS", false);
+            int automatonTimeout = bundle.getInt("com.browser2app.AUTOMATON_TIMEOUT", 30);
+            boolean skipExitPage = bundle.getBoolean("com.browser2app.SKIP_EXIT_PAGE", false);
 
-		khenshin = new Khenshin.KhenshinBuilder()
-				.setApplication(this)
-				.setTaskAPIUrl("https://cmr.browser2app.com/api/automata/")
-				.setDumpAPIUrl("https://cmr.browser2app.com/api/automata/")
-				//.setTaskAPIUrl("https://khipu.com/app/2.0/")
-				//.setDumpAPIUrl("https://khipu.com/cerebro/")
-				.setMainButtonStyle(Khenshin.CONTINUE_BUTTON_IN_FORM)
-				.setAllowCredentialsSaving(true)
-				.setHideWebAddressInformationInForm(false)
-				.setAutomatonTimeout(50)
-				.setSkipExitPage(false)
-				.build();
+            int buttonStyle = Khenshin.CONTINUE_BUTTON_IN_FORM;
+            if (mainButtonStyle.equals("CONTINUE_BUTTON_IN_KEYBOARD")) {
+                buttonStyle = Khenshin.CONTINUE_BUTTON_IN_KEYBOARD;
+            } else if (mainButtonStyle.equals("CONTINUE_BUTTON_IN_TOOLBAR")) {
+                buttonStyle = Khenshin.CONTINUE_BUTTON_IN_TOOLBAR;
+            }
+
+            khenshin = new Khenshin.KhenshinBuilder()
+                    .setApplication(this)
+                    .setTaskAPIUrl(taskApiUrl)
+                    .setDumpAPIUrl(dumpApiUrl)
+                    .setMainButtonStyle(buttonStyle)
+                    .setAllowCredentialsSaving(allowCredentialsSaving)
+                    .setHideWebAddressInformationInForm(hideWebAddress)
+                    .setAutomatonTimeout(automatonTimeout)
+                    .setSkipExitPage(skipExitPage)
+                    .build();
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Failed to load meta-data, NameNotFound: " + e.getMessage());
+        } catch (NullPointerException e) {
+            Log.e(TAG, "Failed to load meta-data, NullPointer: " + e.getMessage());
+        }
+        super.onCreate();
+    }
+
+	public CordovaKhenshinApplication() {
 	}
 
 
--- src/android/com/browser2app/KhenshinPlugin.java
+++ src/android/com/browser2app/KhenshinPlugin.java
@@ -9,13 +9,22 @@ import com.browser2app.khenshin.KhenshinConstants;
 import com.browser2app.khenshin.KhenshinApplication;
 import android.content.Intent;
 import android.os.Bundle;
-import java.util.UUID;
 import java.util.Map;
 import java.util.HashMap;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class KhenshinPlugin extends CordovaPlugin {
 
 	private static final int START_PAYMENT_REQUEST_CODE = 101;
+
+    private CallbackContext pendingCallback;
+
+    @Override
+    public void pluginInitialize() {
+        cordova.setActivityResultCallback(this);
+    }
 
 	public KhenshinPlugin() {
 
@@ -25,6 +34,7 @@ public class KhenshinPlugin extends CordovaPlugin {
 		CordovaArgs cordovaArgs = new CordovaArgs(args);
 		if("startByPaymentId".equals(action)) {
 			startByPaymentId(cordovaArgs.getString(0));
+            this.pendingCallback = callbackContext;
 			return true;
 		} else if ("startByAutomatonId".equals(action)) {
 			Map<String, String> params = new HashMap();
@@ -33,6 +43,7 @@ public class KhenshinPlugin extends CordovaPlugin {
 				params.put(kv[0], kv[1]);
 			}
 			startByAutomatonId(cordovaArgs.getString(0), params);
+            this.pendingCallback = callbackContext;
 			return true;
 		}
 		return false;
@@ -48,6 +59,7 @@ public class KhenshinPlugin extends CordovaPlugin {
 	}
 
 	void startByAutomatonId(String automatonId, Map<String, String> map) {
+
 		Intent intent = ((KhenshinApplication)cordova.getActivity().getApplicationContext()).getKhenshin().getStartTaskIntent();
 		intent.putExtra(KhenshinConstants.EXTRA_AUTOMATON_ID, automatonId);
 		Bundle params = new Bundle();
@@ -57,23 +69,25 @@ public class KhenshinPlugin extends CordovaPlugin {
 			params.putString(entry.getKey(), entry.getValue());
 		}
 
-		/*
-		params.putString("subject", "Pago prueba");
-		params.putString("amount", "2000");
-		params.putString("merchant", "Comercio de prueba");
-		params.putString("paymentId", UUID.randomUUID().toString());
-		params.putString("khipu_account_name", "PAGOCMR");
-		params.putString("khipu_account_number", "55200104571");
-		params.putString("khipu_alias", "PAGOCMR");
-		params.putString("payer_name", "Emilio Davis");
-		params.putString("payer_email", "cmr@khipu.com");
-		params.putString("khipu_rut", "10.706.077-4");
-		params.putString("khipu_email", "transferencias@khipu.com");
-		*/
-
 		intent.putExtra(KhenshinConstants.EXTRA_AUTOMATON_PARAMETERS, params);
 		intent.putExtra(KhenshinConstants.EXTRA_FORCE_UPDATE_PAYMENT, false);
 		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		cordova.getActivity().startActivityForResult(intent, START_PAYMENT_REQUEST_CODE);
+	}
+	@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	 	if (requestCode == START_PAYMENT_REQUEST_CODE) {
+	 		String exitUrl = data.getStringExtra(KhenshinConstants.EXTRA_INTENT_URL);
+	 		if (resultCode == RESULT_OK) {
+				this.pendingCallback.success();
+	 		} else if (resultCode == RESULT_CANCELED) {
+                this.pendingCallback.error("RESULT_CANCELED");
+			} else {
+                this.pendingCallback.error(resultCode);
+            }
+		} else {
+            this.pendingCallback.error(resultCode);
+        }
+
 	}
 }
